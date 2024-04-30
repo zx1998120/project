1c5559758456ee720d043e75ec4bfa70d1c331b0
zx1998120 <73517275+zx1998120@users.noreply.github.com>
2024-04-30 15:45:40 -0400

    Update FlightApplication.java

--- backend/src/main/java/com/example/FlightApplication.java
+++ backend/src/main/java/com/example/FlightApplication.java
@@ -2,6 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FlightApplication {
@@ -10,4 +14,14 @@ public class FlightApplication {
         SpringApplication.run(FlightApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
+
